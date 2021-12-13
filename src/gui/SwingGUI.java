cb167c504123abb2ad9bb943db02196bb048557a
Samuel Mancini (4BHEL) <samuel.mancini@htl-klu.at>
2021-12-13 11:22:48 +0100

    hello2

--- src/gui/SwingGUI.java
+++ src/gui/SwingGUI.java
@@ -5,4 +5,9 @@ import javax.swing.*;
 public class SwingGUI {
     private JTextField textField1;
     private JButton button1;
-}
+    private JButton button2;
+    private JTextField textField2;
+    private JTextField textField3;
+
+
+    }
